46d31b6c0deeaf1f5c84272f2916954a717a993d
andres <andres.guevara03@uceva.edu.co>
2025-05-27 18:05:21 -0500

    CrossOrigin y url explicita para programa

--- src/main/java/co/edu/uceva/semestreservice/delivery/rest/SemestreRestController.java
+++ src/main/java/co/edu/uceva/semestreservice/delivery/rest/SemestreRestController.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/semestre-service")
 public class SemestreRestController {
     private final ISemestreService semestreService;
--- src/main/java/co/edu/uceva/semestreservice/domain/service/IProgramaClient.java
+++ src/main/java/co/edu/uceva/semestreservice/domain/service/IProgramaClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "programa-service")
+@FeignClient(name = "programa-service", url = "http://api.mewings.joptionpane.software")
 public interface IProgramaClient {
     @GetMapping("api/v1/programa-service/programas")
     ResponseEntity<Map<String, Object>> getProgramas();
